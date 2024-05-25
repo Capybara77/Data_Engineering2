0b714e8e29f6f7c9d69319156136200867faf82b
Capybara77 <andrei7318@yandex.ru>
2024-05-25 23:49:53 +0300

    правки по ревью

--- model_test.py
+++ model_test.py
@@ -1,25 +1,40 @@
 import unittest
-from ultralyticsplus import YOLO, render_result
+from ultralyticsplus import YOLO
 
+
+# Определение класса тестирования для обнаружения объектов
 class TestObjectDetection(unittest.TestCase):
 
+    # Метод, который выполняется перед каждым тестом
     def setUp(self):
+        # Инициализация модели YOLO с предобученными весами "best.pt"
         self.model = YOLO("best.pt")
 
+    # Тест для проверки обнаружения объектов на изображении
     def test_detection(self):
-        image_path = "image.jpg"
+        image_path = "image.jpg"  # Путь к тестовому изображению
+        # Получение результатов предсказания модели
         results = self.model.predict(image_path)
 
-        self.assertGreater(len(results[0].boxes), 0, "Модель не обнаружила объектов на изображении.")
+        # Проверка, что модель обнаружила хотя бы один объект на изображении
+        self.assertGreater(len(results[0].boxes), 0,
+                           "Модель не обнаружила объектов на изображении.")
 
+    # Тест для проверки обнаружения объектов любого класса на изображении
     def test_detection_any_class(self):
-        image_path = "image.jpg"
+        image_path = "image.jpg"  # Путь к тестовому изображению
+        # Получение результатов предсказания модели
         results = self.model.predict(image_path)
 
-        self.assertGreater(len(results[0].boxes), 0, "Модель не обнаружила объектов на изображении.")
+        # Проверка, что модель обнаружила хотя бы один объект на изображении
+        self.assertGreater(len(results[0].boxes), 0,
+                           "Модель не обнаружила объектов на изображении.")
 
+    # Метод, который выполняется после каждого теста
     def tearDown(self):
-        pass
+        pass  # Здесь можно освободить ресурсы, если это необходимо
 
+
+# Запуск тестов, если этот файл запускается как главный модуль
 if __name__ == "__main__":
     unittest.main()
